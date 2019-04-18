ce008bf7d7bc47690b3b3dfa923929b5f191298b
Kartic Gharami <dev.kartic@gmail.com>
2019-04-19 02:13:29 +0600

    Exercise on Static Level 3

--- php/index.php
+++ php/index.php
@@ -1,24 +1,30 @@
-<?php
-include 'includes/header.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Learning Point</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <style>
+        .mainContent{
+            margin-top: 5px;
+            padding: 20px 20px 20px 20px;
+            height: 607px;
+            word-wrap: break-word;
+            font-size: 16px;
+        }
+    </style>
+</head>
+<body>
 
+<div class="container">
+    <div class="col-md-10 col-md-offset-1 well mainContent">
+        <?php include 'main.php';?>
+    </div>
 
+</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-include 'includes/footer.php';
+</body>
+</html>
---
+++ php/static.php
@@ -0,0 +1,109 @@
+<?php
+
+/* Exercise : 1
+ Here statically accessed property prefer property of the class for which it is called. Where
+as self keyword enforces use of current class only. Refer the below example:
+*/
+
+class a{
+
+    static protected $test = "class a";
+
+    public function static_test(){
+        echo static::$test; // Results: class b
+        echo '<br>';
+        echo self::$test; // Results: class a
+    }
+}
+
+class b extends a{
+    static protected $test="class b";
+}
+
+$obj = new b();
+$obj->static_test();
+
+/*
+  Output:
+    Class B
+    Class A
+*/
+
+/* Exercise : 2
+It is important to understand the behavior of static properties in the context of class inheritance:
+
+- Static properties defined in both parent and child classes will hold DISTINCT values for each class.
+Proper use of self:: vs. static:: are crucial inside of child methods to reference the intended static property.
+
+- Static properties defined ONLY in the parent class will share a COMMON value.
+*/
+class Foo {
+    static $parent_only;
+    static $both_distinct;
+
+    function __construct() {
+        static::$parent_only = 'fromparent';
+        static::$both_distinct = 'fromparent';
+    }
+}
+
+class Bar extends Foo {
+    static $child_only;
+    static $both_distinct;
+
+    function __construct() {
+        static::$parent_only = 'fromchild';
+        static::$both_distinct = 'fromchild';
+        static::$child_only = 'fromchild';
+    }
+}
+
+$a = new Foo;
+$a = new Bar;
+
+echo 'Parent: parent_only=', Foo::$parent_only, ', both_distinct=', Foo::$both_distinct, "<br/>";
+echo 'Child:  parent_only=', Bar::$parent_only, ', both_distinct=', Bar::$both_distinct, ', child_only=', Bar::$child_only, "<br/>";
+
+/*
+  Output:
+    Parent: parent_only=fromchild, both_distinct=fromparent
+    Child: parent_only=fromchild, both_distinct=fromchild, child_only=fromchild
+*/
+
+/* Exercise: 3
+On PHP 5.2.x or previous you might run into problems initializing static variables in subclasses
+ due to the lack of late static binding:
+*/
+
+class Foo {
+    protected static $a;
+
+    public static function init($value) {
+        self::$a = $value;
+    }
+
+    public static function getA() {
+        return self::$a;
+    }
+}
+
+class Bar extends Foo {
+    protected static $a; // redefine $a for own use
+
+    // inherit the init() method
+    public static function getA() {
+        return self::$a;
+    }
+}
+
+Bar::init('Developer');
+echo 'Foo::$a = '.Foo::getA().'<br/>Bar::$a = '.Bar::getA();
+
+/*
+ If the init() method looks the same for (almost) all subclasses there should be no need to implement init()
+in every subclass and by that producing redundant code. 
+*/
+
+
+
+
