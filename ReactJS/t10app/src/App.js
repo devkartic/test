11d1d31a0d15fd26afef80a6e2c7b19aaf4418f5
Kartic Gharami <dev.kartic@gmail.com>
2019-04-25 00:23:02 +0600

    React Learning 19

--- ReactJS/t10app/src/App.js
+++ ReactJS/t10app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
 
@@ -8,16 +9,34 @@ class App extends Component {
       {id : 1, name : 'Kartic', age : 27, job : 'Software Engineer'},
       {id : 2, name : 'Bablu', age : 30, job : 'Software Developer'},
       {id : 3, name : 'Atik', age : 35, job : 'Developer'},
-      {id : 4, name : 'Bapon', age : 33, job : 'Graphic Designer'}
     ]
   };
+
+  addNinja = (ninja) =>{
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja] // Spread Operator Use here
+    this.setState({
+      ninjas : ninjas
+    })
+  }
+
+  deleteNinja = (id) =>{
+      let ninjas = [this.state.ninjas.filter(ninja =>{
+        return ninja.id !==id;
+      })]
+
+      this.setState({
+        ninjas : ninjas
+      })
+  }
 
   render() {
     return (
       <div className="App">
         <h1>My First React App</h1>
         <p>Welcome :)</p>
-        <Ninjas ninjas = { this.state.ninjas } />
+        <Ninjas deleteNinja={ this.deleteNinja } ninjas = { this.state.ninjas } />
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
--- ReactJS/t10app/src/Ninjas.js
+++ ReactJS/t10app/src/Ninjas.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Ninjas = ({ninjas}) => {
+const Ninjas = ({ninjas, deleteNinja}) => {
 
     const ninjaList = ninjas.map(ninja => {
         if(ninja.age>30){
@@ -10,6 +10,7 @@ const Ninjas = ({ninjas}) => {
                 <div>Name : { ninja.name }</div>
                 <div>Age : { ninja.age }</div>
                 <div>Age : { ninja.job }</div>
+                <button onClick={() => {deleteNinja(ninja.id)}}>Delete Ninja</button> 
             </div>
             )
         } else {
